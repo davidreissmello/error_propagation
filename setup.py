cd45198f7b31824cc63bf55169ddb406ec6119dd
davidreissmello <davidreissmello@gmail.com>
2021-09-14 22:00:47 -0400

    add package information

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="error_propagation",
+    version="0.0.1",
+    author="David Reiss-Mello",
+    author_email="davidreissmello@gmail.com",
+    description="Package that computes error propagation",
+    url="https://github.com/davidreissmello/error_propagation",
+    packages=find_packages(where=""),
+)
